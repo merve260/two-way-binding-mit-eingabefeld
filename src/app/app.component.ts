79b25ac8b4f076f436b017a79e93eb7251185773
merve260 <merve260@gmail.com>
2025-06-17 00:03:29 +0200

    features

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { TwowayBindingComponent } from './compoonents/twoway-binding/twoway-binding.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, FormsModule,TwowayBindingComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
